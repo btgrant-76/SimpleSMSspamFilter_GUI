27af0b982f9a43bea428effaded6dd630846e890
Brian Grant <brian@skeletonjack.net>
2021-02-14 11:52:50 -0600

    Completed the video

--- src/app.py
+++ src/app.py
@@ -2,18 +2,32 @@
 from dearpygui.core import *
 from dearpygui.simple import *
 
-pred = []
+from functions import categorize_words, pre_process, predict
 
+pred = []  # any time I included this as the 3rd parameter for #check_spam, it was always None
 
 def check_spam(sender, data):
-    if pred == []:
-        input_value = get_value('Input')
-        pred.append(input_value)
-        print(f'scenario #1: {input_value}')
-    else:
-        input_value = get_value('Input')
-        pred.append(input_value)
-        print(f'scenario #2: {input_value}')
+    with window('Simple SMS Spam Filter'):
+        if pred == []:
+            add_spacing(count=12)
+            add_separator()
+            add_spacing(count=12)
+
+            input_value = get_value('Input')
+            input_value = pre_process(input_value)
+            pred_text, text_color = predict(input_value)
+
+            pred.append(pred_text)
+            add_text(pred[-1], color=text_color)
+        else:
+            hide_item(pred[-1])
+
+            input_value = get_value('Input')
+            input_value = pre_process(input_value)
+            pred_text, text_color = predict(input_value)
+
+            pred.append(pred_text)
+            add_text(pred[-1], color=text_color)
 
 
 # Window object settings
